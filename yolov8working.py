9b85a58a4c93fea47bdcf9816c590ee12dd6c5da
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2023-07-20 21:21:33 -0400

    videocapture working with yolov8. Time to push.

--- yolov8working.py
+++ yolov8working.py
@@ -7,25 +7,29 @@ model = YOLO('yolov8n.pt')
 
 img = cv2.imread("dog.jpeg")
 
-# Perform prediction on the input image
-results = model.predict(
-   source='https://media.roboflow.com/notebooks/examples/dog.jpeg',
-   conf=0.25
-)
 
-result = results[0]
-classes = result.names
 
-for box in result.boxes:
-   x1, y1, x2, y2 = list(map(int, box.xyxy[0].tolist()))
-   class_id = box.cls[0].item()
-   conf = box.conf[0].item()
+cap = cv2.VideoCapture(0)
+while 1:
+   success, img = cap.read()
+   # Perform prediction on the input image
+   results = model.predict(
+      source=img,
+      conf=0.25
+   )
 
-   obje = classes[class_id]
-   
-   cv2.rectangle(img, (x1, y1), (x2, y2), (255,0,0), 2)
-   cv2.putText(img, f"/{obje}, {conf:.2f}", (x1, y1), cv2.FONT_HERSHEY_COMPLEX, .5, (0, 255, 255), 1)
+   result = results[0]
+   classes = result.names
 
-cv2.imshow("img", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+   for box in result.boxes:
+      x1, y1, x2, y2 = list(map(int, box.xyxy[0].tolist()))
+      class_id = box.cls[0].item()
+      conf = box.conf[0].item()
+
+      obje = classes[class_id]
+      
+      cv2.rectangle(img, (x1, y1), (x2, y2), (255,0,0), 2)
+      cv2.putText(img, f"/{obje}, {conf:.2f}", (x1, y1), cv2.FONT_HERSHEY_COMPLEX, .5, (0, 255, 255), 1)
+
+   cv2.imshow("img", img)
+   cv2.waitKey(1)
