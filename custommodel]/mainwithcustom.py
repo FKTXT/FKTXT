21854eaf4e27e8e9e190f489bffa529c23802d51
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2023-08-07 22:09:09 -0400

    added script with custom model

--- custommodel]/mainwithcustom.py
+++ custommodel]/mainwithcustom.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-from HandTracker import HandDetector
 import os
 from ultralytics import YOLO
 from datetime import datetime
@@ -12,7 +11,7 @@ from datetime import datetime
 # Load the YOLO model
 
 
-model = YOLO('yolov8n.pt')
+model = YOLO('W:/Code/FKTXT/FKTXT/runs/detect/hpd7/weights/best.pt')
 
 # Load image
 
@@ -33,8 +32,6 @@ def checkCoords(landmarks:list, ox, oy, w, h):
 
 
 
-
-handdetector = HandDetector()
 cap = cv2.VideoCapture(0)
 while cap.isOpened():
     success, img = cap.read()
@@ -46,8 +43,6 @@ while cap.isOpened():
 
     h, w, _ = img.shape
 
-    handdetector.findHands(img, draw=False)
-    landmarks = handdetector.find_Landmarks(img, draw=False)
 
     results = model.predict(
         source=img,
@@ -63,9 +58,6 @@ while cap.isOpened():
         x1, y1, x2, y2 = list(map(int, box.xyxy[0].tolist()))
         class_id = box.cls[0].item()
 
-        if class_id != 67:
-            continue
-
         phones_detected +=1
         conf = box.conf[0].item()
 
@@ -74,23 +66,18 @@ while cap.isOpened():
         cv2.rectangle(img, (x1, y1), (x2, y2), (0,255,0), 2)
         cv2.putText(img, f"/{obje}, {conf:.2f}", (x1, y1), cv2.FONT_HERSHEY_COMPLEX, .5, (255, 0, 255), 1)
 
-        if landmarks:
-            if checkCoords(landmarks, x1, y1, x2-x1, y2-y1):
-                print("INJECT")
-                currenttime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-                cv2.putText(img, f"hands detected on phone", (20, 20), cv2.FONT_HERSHEY_SIMPLEX, .6, (0, 0, 255), 2)
-                cv2.putText(img, f"{currenttime}", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, .6, (0, 0, 255), 2)
 
-                # Save the image to the directory with a specific file name (e.g., 'saved_image.jpg')
-                output_directory = 'capturedimages/'
-                output_file_path = output_directory + f'{currenttime}.jpg'
-                # cv2.imwrite(output_file_path, img)
-        else:
-            print("no hands")
+        print("INJECT")
+        currenttime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+        cv2.putText(img, f"hands detected on phone", (20, 20), cv2.FONT_HERSHEY_SIMPLEX, .6, (0, 0, 255), 2)
+        cv2.putText(img, f"{currenttime}", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, .6, (0, 0, 255), 2)
 
-    # Display the output
-    handdetector.findHands(img, draw=True)
-    landmarks = handdetector.find_Landmarks(img, draw=True)
+        # Save the image to the directory with a specific file name (e.g., 'saved_image.jpg')
+        output_directory = 'capturedimages/'
+        output_file_path = output_directory + f'{currenttime}.jpg'
+        # cv2.imwrite(output_file_path, img)
+
+
     cv2.putText(img, f"phones detected: {phones_detected}", (20, h - 20), cv2.FONT_HERSHEY_COMPLEX, .6, (20, 255, 0), 2)
     cv2.imshow("Phone Detection", img)
     if cv2.waitKey(25) & 0xFF == ord('q'):
